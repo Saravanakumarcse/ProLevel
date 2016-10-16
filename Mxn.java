57d22122126738779588d1effbbed93207562876
Saravanakumarcse <manosaravanan31@gmail.com>
2016-10-16 06:55:14 -0700

    Create Mxn.java

---
+++ Mxn.java
@@ -0,0 +1,15 @@
+class Mxn
+{
+public void cal(int a,int b,double c)
+{
+	double vol;
+	vol=a*b*c;
+	System.out.println("volume is"+(vol));
+}
+	public static void main(String[] args) {
+		
+Mxn m=new Mxn();
+m.cal(30,40, 8.9);
+	}
+
+}
